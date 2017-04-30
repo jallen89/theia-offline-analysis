a078b43314462416e07cdb4d41acd068b3d7c034
yang <yang.ji@gatech.edu>
2017-04-30 16:35:00 -0400

    use query interface

--- analysis-agent/Theia_Tagging/test.py
+++ analysis-agent/Theia_Tagging/test.py
@@ -9,14 +9,17 @@ print '(' + sys.argv[0] + ')'
 print '(' + sys.argv[1] + ')'
 print '(' + sys.argv[2] + ')'
 print '(' + path + ')'
-p = subprocess.Popen(['/home/yang/omniplay/test/resume', 
+p = subprocess.Popen(['/home/theia/theia-es/test/resume', 
                       path,'-p', '--pthread', 
-                      '/home/yang/omniplay/eglibc-2.15/prefix/lib'])
+                      '/home/theia/theia-es/eglibc-2.15/prefix/lib'])
 print p.pid
 time.sleep(5)
-#q = subprocess.Popen(['/home/yang/Downloads/pin-2.13/pin', '-pid', str(p.pid), '-t',
-#                      '/home/yang/omniplay/pin_tools/obj-ia32/print_instructions.so'])
-q = subprocess.Popen(['/home/yang/omniplay/pin_tools/dtracker/pin/pin', '-pid', str(p.pid), '-t',
-                      '/home/yang/omniplay/pin_tools/dtracker/obj-ia32/dtracker.so'])
+
+q = subprocess.Popen(['/home/theia/theia-es/pin_tools/dtracker/pin/pin', '-pid',
+										  str(p.pid), '-t', '/home/theia/theia-es/pin_tools/dtracker/obj-ia32/dtracker.so', 
+											'-no_neo4j', 'true', '-tag_count_file', '/home/theia/tags.txt', '-publish_to_kafka', 
+											'true', '-kafka_server', '10.0.50.19:9092', '-kafka_topic', 'ta1-theia-qr', 
+											'-create_avro_file', 'true', '-avro_file', '/home/theia/1_tags.bin', '-query_id', '1', 
+											'-use_source_id', 'true', '-source_id', '700000269']);
 
 
